cc54a9720410a908f8c351fe71c074363864edc2
Roman <60231794+RomanLavryniv@users.noreply.github.com>
2022-02-22 12:18:59 +0200

    added toggle buttons (up, down)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const sidebar = document.querySelector('.sidebar');
 const container = document.querySelector('.container');
 
 const mainSlide = document.querySelector('.main-slide');
+
 const slidesCount = mainSlide.querySelectorAll('div').length;
 
 let activeSlideIndex = 0;
@@ -21,20 +22,26 @@ downBtn.addEventListener('click', () => {
     changeSlide('down')
 });
 
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up')
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down')
+    }
+});
+
 function changeSlide(direction) {
     if (direction === 'up') {
         activeSlideIndex++
         if (activeSlideIndex === slidesCount) {
             activeSlideIndex = 0
         }
-    }
-    else if (direction === 'down') {
+    } else if (direction === 'down') {
         activeSlideIndex--
         if (activeSlideIndex < 0) {
             activeSlideIndex = slidesCount - 1
         }
     }
-
     const height = container.clientHeight
 
     mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
